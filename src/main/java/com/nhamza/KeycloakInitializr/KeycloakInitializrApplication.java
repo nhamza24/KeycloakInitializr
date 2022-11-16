a871ddf6644f2569c9f972174433027e18d7edff
Nabil Hamza <nabil.hamza@atos.net>
2022-11-16 10:36:01 +0100

    initial commit

---
+++ src/main/java/com/nhamza/KeycloakInitializr/KeycloakInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.nhamza.KeycloakInitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakInitializrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhamza/KeycloakInitializr/KeycloakInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhamza.KeycloakInitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
